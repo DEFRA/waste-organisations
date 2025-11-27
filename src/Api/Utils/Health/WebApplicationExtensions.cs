aef8ae279f1871902391cf925c66ceeaacc7c547
Tim Peel <tim.peel@equalexperts.com>
2025-11-27 12:10:29 +0000

    Update pipeline and enable Sonar

---
+++ src/Api/Utils/Health/WebApplicationExtensions.cs
@@ -0,0 +1,79 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Api.Utils.Health;
+
+[ExcludeFromCodeCoverage]
+public static class WebApplicationExtensions
+{
+    private static readonly JsonSerializerOptions s_options = new();
+
+    public const string Ready = "ready";
+    public const string Extended = "extended";
+
+    public static void MapHealth(this WebApplication app)
+    {
+        app.MapHealthChecks(
+                "/health",
+                new HealthCheckOptions { Predicate = healthCheck => healthCheck.Tags.Contains(Ready) }
+            )
+            .AllowAnonymous();
+
+        app.MapHealthChecks(
+                "/health/all",
+                new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = (context, report) =>
+                    {
+                        context.Response.ContentType = "application/json; charset=utf-8";
+
+                        var options = new JsonWriterOptions { Indented = true };
+
+                        using var memoryStream = new MemoryStream();
+                        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+                        {
+                            jsonWriter.WriteStartObject();
+                            jsonWriter.WriteString("status", report.Status.ToString());
+                            jsonWriter.WriteStartObject("results");
+
+                            foreach (var entry in report.Entries)
+                            {
+                                jsonWriter.WriteStartObject(entry.Key);
+                                jsonWriter.WriteString("status", entry.Value.Status.ToString());
+
+                                if (entry.Value.Description != null)
+                                    jsonWriter.WriteString("description", entry.Value.Description);
+
+                                if (entry.Value.Exception != null)
+                                    jsonWriter.WriteString("exception", entry.Value.Exception.Message);
+
+                                if (entry.Value.Data.Any())
+                                {
+                                    jsonWriter.WriteStartObject("data");
+
+                                    foreach (var item in entry.Value.Data)
+                                    {
+                                        jsonWriter.WritePropertyName(JsonNamingPolicy.CamelCase.ConvertName(item.Key));
+                                        JsonSerializer.Serialize(jsonWriter, item.Value, s_options);
+                                    }
+
+                                    jsonWriter.WriteEndObject();
+                                }
+
+                                jsonWriter.WriteEndObject();
+                            }
+
+                            jsonWriter.WriteEndObject();
+                            jsonWriter.WriteEndObject();
+                        }
+
+                        return context.Response.WriteAsync(Encoding.UTF8.GetString(memoryStream.ToArray()));
+                    },
+                }
+            )
+            .AllowAnonymous();
+    }
+}
