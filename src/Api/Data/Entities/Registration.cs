981b128fb98aef79d993d100a2e79b351176712d
Tim Peel <tim.peel@equalexperts.com>
2025-12-08 11:40:53 +0000

    PR feedback 2

--- src/Api/Data/Entities/Registration.cs
+++ src/Api/Data/Entities/Registration.cs
@@ -3,6 +3,6 @@ namespace Api.Data.Entities;
 public record Registration
 {
     public required string Type { get; init; }
-    public int RegistrationYear { get; init; }
+    public required int RegistrationYear { get; init; }
     public required string Status { get; init; }
 }
